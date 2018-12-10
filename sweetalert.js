bc17611f57bc15b0f88104f01b7f56f0f731ecad
chaanto <charlieaprilianto52@gmail.com>
2018-12-10 13:38:27 +0700

    ea

---
+++ sweetalert.js
@@ -0,0 +1,10 @@
+function sweet(){
+    swal ("Good job!", "You clicked the button", "success");
+}
+
+function swal2(){
+    swal({
+        title: 'Oops...',
+        text: 'Something went Wrong!'
+    })
+}
